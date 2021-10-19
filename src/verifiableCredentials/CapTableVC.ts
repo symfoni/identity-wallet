fddaf60f4540b0bfd135a55a95a7dd489e5207fe
Jonas Johan Solsvik <jonas@symfoni.solutions>
2021-10-19 15:13:03 +0200

    Add CapTableVC

---
+++ src/verifiableCredentials/CapTableVC.ts
@@ -0,0 +1,17 @@
+import { VerifiableCredential } from "@veramo/core";
+import { CapTable } from "../types/createCapTableVPTypes";
+
+// @see https://www.notion.so/symfoni/NationalIdentityVC-f0ad0a6b75a64cca9d887f0243dc41ae
+export type CapTableVC = VerifiableCredential & {
+    "@context": [
+        "https://www.w3.org/2018/credentials/v1",
+        "https://www.symfoni.dev/credentials/v1"
+    ];
+    type: ["VerifiableCredential", "CapTableVC"];
+    issuer: string;
+    credentialSubject: {
+        capTable: CapTable;
+    };
+    issuanceDate: string;
+    expirationDate: string;
+};
