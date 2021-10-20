35c774affbcc0e283624ee9bdf82a379b4dcad47
Jonas <jonas@symfoni.solutions>
2021-10-20 09:20:18 +0200

    Apply suggestions from code review

--- src/domain/brok-helpers.ts
+++ src/domain/brok-helpers.ts
@@ -27,7 +27,7 @@ export const registerWithBankId = (vp: VerifiablePresentation) => {
 export const requestBoardDirectorVerifiableCredential = (
     vp: VerifiablePresentation
 ) => {
-    const url = !USE_LOCAL_ENVIROMENT
+    const url = !!USE_LOCAL_ENVIROMENT
         ? "http://localhost:3004"
         : BROK_HELPERS_URL;
     return axios.post<string>(`${url}/brreg/credential/board-director`, {
@@ -40,7 +40,7 @@ export const approveCaptable = (
     vp: VerifiablePresentation,
     options: { test?: false } = {}
 ) => {
-    const url = !USE_LOCAL_ENVIROMENT
+    const url = !!USE_LOCAL_ENVIROMENT
         ? "http://localhost:3004"
         : BROK_HELPERS_URL;
     return axios.post<string>(`${url}/brreg/captable/approve`, {
