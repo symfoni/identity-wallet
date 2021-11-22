bc1242d0cf44c9a30e5dca4272bf4bc452a46882
Jonas Johan Solsvik <jonas@symfoni.solutions>
2021-11-22 13:39:21 +0100

    remove unused envvars

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,13 +1,8 @@
 // @see https://levelup.gitconnected.com/using-environment-variables-in-a-react-native-app-f2dd005d2457
 declare module "@env" {
-    export const API_BASE: string;
     export const BANKID_CALLBACK_URL: string;
     export const BANKID_CLIENT_ID: string;
     export const BANKID_ACR_VALUES: string;
     export const BANKID_URL: string;
-    export const BROK_HELPERS_URL: string;
-    export const BROK_HELPERS_VERIFIER: string;
-    export const USE_LOCAL_ENVIROMENT: boolean;
-    export const USE_TEST_DATA: boolean;
     export const APP_ENV: "dev" | "stage" | "prod";
 }
